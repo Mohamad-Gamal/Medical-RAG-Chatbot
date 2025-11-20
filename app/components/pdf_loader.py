c18ac79e60152d91dfd15a5338afe054270551b2
MohamadGamal <108193760+Mohamad-Gamal@users.noreply.github.com>
2025-11-20 20:55:45 +0000

    loader

--- app/components/pdf_loader.py
+++ app/components/pdf_loader.py
@@ -3,7 +3,21 @@ from langchain_community.document_loaders import DirectoryLoader, PyPDF_Loader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from app.common.logger import get_logger
-from app.common.custom_exception import Custom_exception
+from app.common.custom_exception import Custom_Exception
 from app.config.config import DATA_PATH, CHUNK_OVERLAP, CHUNK_SIZE
 
 logger = get_logger(__name__)
+
+def load_pdf_files():
+    try:
+        if not os.path.exists(DATA_PATH):
+            raise Custom_Exception("Data Path does not exist")
+
+        logger.info(f"Loading files from{DATA_PATH}")
+
+        loader = DirectoryLoader(DATA_PATH,glob="*.pdf",loader_cls=PyPDF_Loader)
+
+        documents = loader.load()
+
+        if not documents:
+            logger.warning("")
