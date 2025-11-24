0ae189149d9434cacefb8fb7e37729007dab1c89
MohamadGamal <108193760+Mohamad-Gamal@users.noreply.github.com>
2025-11-24 17:28:40 +0000

    update

--- app/components/llm.py
+++ app/components/llm.py
@@ -1,4 +1,6 @@
 from langchain_huggingface import HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import HumanMessage  # For prompt formatting
 from app.config.config import HF_TOKEN, HUGGINGFACE_REPO_ID
 from app.common.logger import get_logger
 from app.common.custom_exception import CustomException
@@ -13,17 +15,17 @@ def load_llm(huggingface_repo_id: str = HUGGINGFACE_REPO_ID, hf_token: str = HF_
             raise CustomException("HF_TOKEN is missing in environment variables.")
 
         # Use HuggingFaceEndpoint with explicit task parameter
-        llm = HuggingFaceEndpoint(
+        endpoint = HuggingFaceEndpoint(
             repo_id=huggingface_repo_id,
             huggingfacehub_api_token=hf_token,
-            task="conversational",  # This is REQUIRED
+            task="text-generation",  # This is REQUIRED
             temperature=0.1,
             max_new_tokens=512,
             top_p=0.9,
             do_sample=True,
             return_full_text=False
         )
-
+        llm = ChatHuggingFace(llm=endpoint)
         logger.info(f"LLM loaded successfully: {huggingface_repo_id}")
         return llm
 
